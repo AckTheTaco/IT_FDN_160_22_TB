4b2d860afbe38d473fe8e1fd9079334dd2df0262
WonderBeard <TonyC.burnett@gmail.com>
2022-10-14 21:49:59 -0700

    Homework Assignments for Session 1

---
+++ Session1/Homework #2/HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main() //Starting point for compiler
+    {
+        Console.WriteLine("Hello World, TonyB!" );
+        Console.ReadLine();
+    }
+}
