0a4155915a969849f18181847ba82827fc358474
WonderBeard <TonyC.burnett@gmail.com>
2022-10-28 00:53:29 -0700

    Finished Session 4 Homework with Factorial Loop

---
+++ Session4/Session4_Work/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session4_Work
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int result = 1;
+            int input = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Enter a samll number: ");
+
+            for (int i = 1; i <= input; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine("{0}! is {1}", input, result);
+            Console.ReadLine(); 
+        }
+    }
+}
--- Session4/Sesssion4/Program.cs
+++ Session4/Sesssion4/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// Write a program that calculates factorial of an integer n given by the user.
